eda46d423ade7c80c44c95c2b371170498460cb9
Mariano Carballal <marianocarballal@gmail.com>
2016-03-23 11:29:35 -0300

    Fix externals

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -66,6 +66,20 @@ module.exports = {
       }
     })
   ],
+  externals: {
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
   module: {
     loaders: [
       {
